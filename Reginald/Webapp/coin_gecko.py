984ab287bde0d6dfcf859a4289e57f7c976595ca
Reginald Robinson <ddrbnsn@gmail.com>
2023-05-31 12:49:43 -0700

    adding api calls to server

---
+++ Reginald/Webapp/coin_gecko.py
@@ -0,0 +1,54 @@
+# %%
+import requests
+import pandas as pd
+
+# %%
+def get_coingecko_coin_data():
+    """
+    Retrieve coin data from the CoinGecko API and return a DataFrame.
+    """
+    # Set up the API endpoint
+    url = 'https://api.coingecko.com/api/v3/'
+
+    # Make a GET request to retrieve the data
+    response = requests.get(url + 'coins/markets', params={
+        'vs_currency': 'usd',
+        'ids': 'bitcoin,ethereum,tether,litecoin,cardano',
+        'order': 'market_cap_desc',
+        'per_page': 10,
+        'page': 1,
+        'sparkline': False
+    })
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Retrieve the response data
+        data = response.json()
+        #create a lists to hold
+        coin_data = []
+        for coin in data:
+            coin_data.append({"Symbol": coin['symbol'], "Price": coin['current_price']})
+            
+        # todays_price = pd.DataFrame(coin_data)
+        # todays_price["Price"] = pd.to_numeric(todays_price["Price"])
+        # todays_price = todays_price.sort_values(by='Price', ascending=False)
+        # todays_price["Price"] = todays_price["Price"].map('${:,.2f}'.format)
+        
+        rename_symbols = {'btc': 'BTCUSD',
+                          'eth' : 'ETHUSD',
+                          'ltc' : 'LTCUSD',
+                          'usdt': 'USDTUSD',
+                          'ada' : 'ADAUSD'}
+        # todays_price['Symbol'] = todays_price['Symbol'].replace(rename_symbols)
+            
+        return coin_data
+            
+            
+
+# %%
+# coin_data = get_coinGecko_coin_data()
+# coin_data
+
+if(__name__ == "__main__"):
+    get_coingecko_coin_data()
+
+
