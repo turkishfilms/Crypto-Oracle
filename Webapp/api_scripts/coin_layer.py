cdc3aa1692f0ae16fbc54cb3e45ce0bc13c4e002
Reginald Robinson <ddrbnsn@gmail.com>
2023-06-03 15:30:28 -0700

    links

--- Webapp/api_scripts/coin_layer.py
+++ Webapp/api_scripts/coin_layer.py
@@ -1,6 +1,7 @@
 import sys
 import requests
 import json
+from datetime import date, timedelta
 
 sys.path.append("../../")  
 
@@ -14,10 +15,22 @@ def get_coinlayer_prices():
 
     response = requests.get(url)
     data = response.json()
-    return json.dumps(data)
+    return data
     
 
+def get_coinlayer_prices_yesterday():
+    api_key = coinlayer_api_key
+    symbols = "BTC,ETH,LTC,USDT,ADA"
+    yesterday = date.today()-timedelta(days=1)
+    yesterdizzle = yesterday.isoformat()
+    url = f"http://api.coinlayer.com/{yesterday}?access_key=" + api_key + "&symbols=" + symbols + "&expand=1"
+
+    response = requests.get(url)
+    data = response.json()
+    return data    
+
 if(__name__=="__main__"):
+    # coins = get_coinlayer_prices()
     coins = get_coinlayer_prices()
     with open("../jsons/clarb.json","w") as j:
         json.dump(coins,j)
