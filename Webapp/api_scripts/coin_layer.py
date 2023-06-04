46be69b8b0eaf76959c3447ad0a0ed90ae1ea6a3
Reginald Robinson <ddrbnsn@gmail.com>
2023-06-03 18:06:54 -0700

    got it finally wokring wo api calls

--- Webapp/api_scripts/coin_layer.py
+++ Webapp/api_scripts/coin_layer.py
@@ -3,15 +3,14 @@ import requests
 import json
 from datetime import date, timedelta
 
-sys.path.append("../../")  
+sys.path.append("../")  
 
 from temp.API_KEYS import coinlayer_api_key
 
 
 def get_coinlayer_prices():
-    api_key = coinlayer_api_key
     symbols = "BTC,ETH,LTC,USDT,ADA"
-    url = "http://api.coinlayer.com/live?access_key=" + api_key + "&symbols=" + symbols
+    url = "http://api.coinlayer.com/live?access_key=" + coinlayer_api_key + "&symbols=" + symbols
 
     response = requests.get(url)
     data = response.json()
@@ -23,7 +22,7 @@ def get_coinlayer_prices_yesterday():
     symbols = "BTC,ETH,LTC,USDT,ADA"
     yesterday = date.today()-timedelta(days=1)
     yesterdizzle = yesterday.isoformat()
-    url = f"http://api.coinlayer.com/{yesterday}?access_key=" + api_key + "&symbols=" + symbols + "&expand=1"
+    url = f"http://api.coinlayer.com/{yesterdizzle}?access_key=" + api_key + "&symbols=" + symbols + "&expand=1"
 
     response = requests.get(url)
     data = response.json()
@@ -31,8 +30,10 @@ def get_coinlayer_prices_yesterday():
 
 if(__name__=="__main__"):
     # coins = get_coinlayer_prices()
+    # sys.path.append("../../")  
+
     coins = get_coinlayer_prices()
-    with open("../jsons/clarb.json","w") as j:
+    with open("../jsons/clarby2.json","w") as j:
         json.dump(coins,j)
     print(coins)
    
