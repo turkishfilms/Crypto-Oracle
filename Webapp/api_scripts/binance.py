5b1dc9acc231798400b0579d5f901246f3e07da3
Reginald Robinson <ddrbnsn@gmail.com>
2023-06-04 14:58:08 -0700

    multicoin support for binance

--- Webapp/api_scripts/binance.py
+++ Webapp/api_scripts/binance.py
@@ -3,33 +3,18 @@ import requests
 import pandas as pd
 import json
 
-def get_binance_prices(coinSymbols):
+def get_binance_prices(coin_symbols):
     """
     Retrieve current prices for specified symbols from Binance API and return a DataFrame.
 
     Returns:
         dict: Dict containing the retrieved prices.
     """
-    symbols = []
-    for symbol in coinSymbols:
-        symbols.append(symbol + "USD")
-
-    # symbols = ["BTCUSD", "ETHUSD", "LTCUSD", "USDTUSD","ADAUSD" ]
     
-    # Make a GET request to retrieve the data from Binance API
-    current_prices = requests.get('https://api.binance.us/api/v3/ticker/price')
-    data = current_prices.json()
-
-    # Collect the prices for the specified symbols
-    prices = {}
-    for item in data:
-        symbol = item['symbol']
-        price = item['price']
-        if symbol in symbols:
-            prices[symbol] = price
-
-    # return prices
-    return {"BTC":float(prices["BTCUSD"]),"LTC":float(prices["LTCUSD"]),"ADA":float(prices["ADAUSD"]),"ETH":float(prices["ETHUSD"]),"USDT":float(prices["USDTUSD"]),}
+    symbols_str = ','.join([f'"{symbol}USD"' for symbol in coin_symbols])
+    req = 'https://api.binance.us/api/v3/ticker/price?symbols=[' + symbols_str + ']'
+    data = requests.get(req).json()
+    return  {coin['symbol'][:-3]: float(coin['price']) for coin in data}
 
 if(__name__ == "__main__"):
     coins = get_binance_prices([])
